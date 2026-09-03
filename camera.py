4a018ee6d437dbe865f5a7b15a3b3b63b3bbc227
tharunkumarM9 <tharun2004mt@gmail.com>
2026-09-03 17:50:30 +0530

    Implement photo capture functionality and integrate with registration process

--- camera.py
+++ camera.py
@@ -6,41 +6,55 @@
 import cv2
 import os
 import datetime
+import numpy as np
+
+UPLOAD_FOLDER = "static/uploads"
+
+
+# def capture_photo():
+#     os.makedirs("static/uploads",exist_ok=True)
+#     camera = cv2.VideoCapture(0) #(open camera) video capture is a method to open the default camera of your syster , here 0 defines the default camera, 1 defines the multiple cameras when we are plug in 
+#     while True:
+#         success,frame = camera.read() #here tells wheather the camera successfull give the frame
+
+#         if not success:
+#             camera.release() #stop the camera
+#             cv2.destroyAllWindows()  #it closes the all window related to open cv
+#             return None
+
+#         cv2.imshow("capture photo",frame) #reads the camera shows photo
+#         key=cv2.waitKey(1) #candidate keyboard events
+
+#         if key==ord("s"):
+#             filename = f"candidate_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.jpg" #nameing convention
+#             photo_path= os.path.join("static/uploads",filename) #here we are joining the photo path 
+#             cv2.imwrite(photo_path,frame) # save the photo in exisiting folder
+#             print("photo captured")
+
+#             camera.release()
+#             cv2.destroyAllWindows()
+#             return photo_path
+#         elif key == ord("q"):
+#             camera.release()  
+#             cv2.destroyAllWindows()
+#             return None
+
+# path = capture_photo()
+# if path:
+#     print("path",path)
+# else:
+#     print("photo captured cancel")
+
 def capture_photo():
-    os.makedirs("static/uploads",exist_ok=True)
-    camera = cv2.VideoCapture(0) #(open camera) video capture is a method to open the default camera of your syster , here 0 defines the default camera, 1 defines the multiple cameras when we are plug in 
-    while True:
-        success,frame = camera.read() #here tells wheather the camera successfull give the frame
-
-        if not success:
-            camera.release() #stop the camera
-            cv2.destroyAllWindows()  #it closes the all window related to open cv
-            return None
-
-        cv2.imshow("capture photo",frame) #reads the camera shows photo
-        key=cv2.waitKey(1) #candidate keyboard events
-
-        if key==ord("s"):
-            filename = f"candidate_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.jpg" #nameing convention
-            photo_path= os.path.join("static/uploads",filename) #here we are joining the photo path 
-            cv2.imwrite(photo_path,frame) # save the photo in exisiting folder
-            print("photo captured")
-
-            camera.release()
-            cv2.destroyAllWindows()
-            return photo_path
-        elif key == ord("q"):
-            camera.release()  
-            cv2.destroyAllWindows()
-            return None
-
-path = capture_photo()
-if path:
-    print("path",path)
-else:
-    print("photo captured cancel")
-
-
+    os.makedirs(UPLOAD_FOLDER,exist_ok=True)
+    image_array=np.frombuffer(image_array,np.uint8)     # convert image bites into array
+    img = cv2.imdecode(image_array,cv2.IMREAD_COLOR)   #convert array into image frame
+    if img is None:
+        return None
+    filename = ("candidate_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.jpg")
+    photo_path = os.path.join("static/uploads",filename)
+    cv2.imwrite(photo_path,img)
+    return photo_path  #saved a photo in existing file
 
 
 
